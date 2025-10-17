b2db2edfde9fc086a32371baaf94aaad4f3d630f
Suganthi-K-Off <suganthi82005@gmail.com>
2025-10-17 10:58:25 +0530

    htmal tags

---
+++ java script basic/sample.js
@@ -0,0 +1,37 @@
+
+
+
+ let costPerKg = 20;
+let quantity = 5;
+let totalCost = costPerKg * quantity;
+console.log("Total cost for " + quantity + " kg of blueberries: " + totalCost + " rupees");  
+//blueberry// */
+
+ var a3= 100
+b3=a3--;
+console.log(a3)                                 
+//script// */
+
+let a=10;
+let b=5;
+let sum=a+b;           
+let difference=a-b; 
+let product=a*b;      
+let quotient=a/b;     
+let remainder=a%b;    
+let increment=a++;         
+let decrement=b--;         
+console.log("Addition:"+sum);
+console.log("Subtraction:"+difference);
+console.log("Multiplication:"+product);
+console.log("Division:"+quotient);
+console.log("Modulus:"+remainder);
+console.log("Increment of a:"+increment);
+console.log("Decrement of b:"+decrement);               
+//operators// */
+
+ var ao=100
+var bo=--a
+console.log(ao)
+console.log(bo)
+console.log(ao)
